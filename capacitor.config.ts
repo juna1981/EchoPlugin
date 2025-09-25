041d17cf88429dd1c308be7fc2a59d19ecb11550
Juan Carlos Quiles <juancarlos1@Mac-mini-de-Juan.local>
2025-09-25 19:45:11 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'blog',
+  webDir: 'www'
+};
+
+export default config;
